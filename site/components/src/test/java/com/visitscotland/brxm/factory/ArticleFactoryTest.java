8a4122810640f59ec8e7622046cf8061716eef7a
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2022-10-17 14:39:58 +0100

    VS-4069 FEAT (Videos) new tests implementation

--- site/components/src/test/java/com/visitscotland/brxm/factory/ArticleFactoryTest.java
+++ site/components/src/test/java/com/visitscotland/brxm/factory/ArticleFactoryTest.java
@@ -54,13 +54,14 @@ class ArticleFactoryTest {
         assertEquals(article, module.getHippoBean());
     }
 
-/*
     @Test
     @DisplayName("VS-4069 Main Article - Populates all fields with video")
     void getModuleWithMainVideo() {
         HstRequest request = new MockHstRequest();
         Article article = mock(Article.class);
-        when(article.getMediaItem()).thenReturn(mock(VideoLink.class));
+        VideoLink videoLink = mock(VideoLink.class);
+        when(videoLink.getVideoLink()).thenReturn(mock(Video.class));
+        when(article.getMediaItem()).thenReturn(videoLink);
         when(article.getTitle()).thenReturn("Title");
         when(article.getAnchor()).thenReturn("Anchor");
         when(article.getCopy()).thenReturn(mock(HippoHtml.class));
@@ -73,7 +74,7 @@ class ArticleFactoryTest {
         assertEquals(HippoHtml.class, module.getIntroduction().getClass());
         assertEquals(article, module.getHippoBean());
     }
-*/
+
 
     @Test
     @DisplayName("Article Sections - Populates all fields")
@@ -93,7 +94,7 @@ class ArticleFactoryTest {
         verify(embedder, times(2)).getQuote(any(Quote.class), any(), any());
         assertEquals(2, module.getSections().size());
     }
-/*
+
     @Test
     @DisplayName("VS-4069 Article Sections - Populates all fields with video")
     void getModuleSectionsWithVideo() {
@@ -101,15 +102,18 @@ class ArticleFactoryTest {
         ArticleSection section = mock(ArticleSection.class);
         Article article = mock(Article.class);
 
+        VideoLink videoLink = mock(VideoLink.class);
+        when(videoLink.getVideoLink()).thenReturn(mock(Video.class));
+        when(section.getMediaItem()).thenReturn(videoLink);
         when(section.getCopy()).thenReturn(mock(HippoHtml.class));
-        when(section.getMediaItem()).thenReturn(mock(Image.class));
         when(section.getQuote()).thenReturn(mock(Quote.class));
         when(article.getParagraph()).thenReturn(Arrays.asList(section, section));
 
+
         ArticleModule module = factory.getModule(request, article);
 
-        verify(imageFactory, times(2)).getImage(any(Image.class), any(), any());
+        verify(linkService, times(2)).createVideo(any(Video.class), any(), any());
         verify(embedder, times(2)).getQuote(any(Quote.class), any(), any());
         assertEquals(2, module.getSections().size());
-    }*/
+    }
 }
