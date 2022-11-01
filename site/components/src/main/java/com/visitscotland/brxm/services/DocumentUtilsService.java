a66652d50c834dea501998d1e72b605db0bc15a6
Jose Calcines <jose.calcines@visitscotland.com>
2022-11-01 12:26:32 +0000

    VS-4090 FEAT (Language) Generated Fallback pages URLs

--- site/components/src/main/java/com/visitscotland/brxm/services/DocumentUtilsService.java
+++ site/components/src/main/java/com/visitscotland/brxm/services/DocumentUtilsService.java
@@ -183,7 +183,7 @@ public class DocumentUtilsService {
         String languagePath = "";
 
         if (locale != null) {
-            languagePath += "/" + locale.getLanguage();
+            languagePath += Language.getLanguageForLocale(locale).getPathVariable();
         }
 
         return properties.getCmsBasePath() +
