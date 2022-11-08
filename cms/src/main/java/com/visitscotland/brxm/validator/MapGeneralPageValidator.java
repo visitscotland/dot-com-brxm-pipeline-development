ee1a3916c794b40457304fc9ebf926b5e033f0e5
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2022-11-08 08:41:49 +0000

    VS-4098 FEAT (Maps) Add validator for taxonomies

---
+++ cms/src/main/java/com/visitscotland/brxm/validator/MapGeneralPageValidator.java
@@ -0,0 +1,48 @@
+package com.visitscotland.brxm.validator;
+
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import java.util.Optional;
+
+/**
+ * jcr:nane = visitscotland:map-general-page-validator
+ */
+public class MapGeneralPageValidator implements Validator<Node> {
+
+    private static final String CMS_LINK_TYPE = "visitscotland:CMSLink";
+    private static final Logger logger = LoggerFactory.getLogger(MapGeneralPageValidator.class);
+
+    @Override
+    public Optional<Violation> validate(ValidationContext validationContext, Node node) {
+        try {
+            NodeIterator itemIterator = node.getNodes("hippotaxonomy:keys");
+            Node firstNode = itemIterator.nextNode();
+            if (itemIterator.getSize() > 1) {
+                // Can't have more than one featured event
+                return Optional.of(validationContext.createViolation("multipleFeaturedEvent"));
+            } else {
+                if (!firstNode.getProperty("visitscotland:productType").getString().equals("even")) {
+                    return Optional.of(validationContext.createViolation("notEvent"));
+                }
+            } else {
+                while (itemIterator.hasNext()) {
+                    Node itemNode = itemIterator.nextNode();
+                    if (!itemNode.isNodeType(CMS_LINK_TYPE)) {
+                        // Can not have ProductSearch mixed with CMSLink
+                        return Optional.of(validationContext.createViolation("mixedFeaturedWidget"));
+                    }
+                }
+            }
+        } catch (RepositoryException ex) {
+            logger.error("Repository error during validation", ex);
+        }
+        return Optional.empty();
+    }
+}
