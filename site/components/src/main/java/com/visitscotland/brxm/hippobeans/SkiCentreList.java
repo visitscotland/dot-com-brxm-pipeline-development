8dbaa275f78ebaf10b50576b52a3477b32adc0f4
Jose Calcines <jose.calcines@visitscotland.com>
2023-01-06 22:32:57 +0000

    VS-4412 (Ski) Complete content modelling for Ski Centres and Grid

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/SkiCentreList.java
@@ -0,0 +1,62 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import java.util.Calendar;
+import java.util.List;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:SkiCentreList")
+@Node(jcrType = "visitscotland:SkiCentreList")
+public class SkiCentreList extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "visitscotland:title")
+    public String getTitle() {
+        return getSingleProperty("visitscotland:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:copy")
+    public HippoHtml getCopy() {
+        return getHippoHtml("visitscotland:copy");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:links", allowModifications = false)
+    public List<HippoBean> getMegalinkItems() {
+        return getLinkedBeans("visitscotland:links", HippoBean.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:diff")
+    public String getDiff() {
+        return getSingleProperty("visitscotland:diff");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translationFlag")
+    public Boolean getTranslationFlag() {
+        return getSingleProperty("visitscotland:translationFlag");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translationPriority")
+    public String getTranslationPriority() {
+        return getSingleProperty("visitscotland:translationPriority");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translationDeadline")
+    public Calendar getTranslationDeadline() {
+        return getSingleProperty("visitscotland:translationDeadline");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:feed")
+    public String getFeed() {
+        return getSingleProperty("visitscotland:feed");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:productId")
+    public String getProductId() {
+        return getSingleProperty("visitscotland:productId");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:introduction")
+    public HippoHtml getIntroduction() {
+        return getHippoHtml("visitscotland:introduction");
+    }
+}
