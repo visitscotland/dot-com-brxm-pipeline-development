05aed643de9b04087c8ae0bdb385d5a265c442e8
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2023-03-22 15:47:37 +0000

    VSD-288 FEAT (Itinerary) Logging if coordinates are null (needed for the map)

--- site/components/src/main/java/com/visitscotland/brxm/factory/ItineraryFactory.java
+++ site/components/src/main/java/com/visitscotland/brxm/factory/ItineraryFactory.java
@@ -202,7 +202,7 @@ public class ItineraryFactory {
 
         if (externalLink.getCoordinates() != null) {
             module.setCoordinates(new Coordinates(externalLink.getCoordinates().getLatitude(), externalLink.getCoordinates().getLongitude()));
-        }else{
+        } else {
             String message = String.format("The DMS product added to '%s' does not have coordinates, please review the DMS Product id field at: %s ", module.getTitle(), externalLink.getPath());
             module.addErrorMessage(message);
             if (logger.isWarnEnabled()) {
