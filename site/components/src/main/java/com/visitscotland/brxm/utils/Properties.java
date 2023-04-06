cf6cea7acb56cd8c7aacc74a0205456795ba4731
Jose Calcines <jose.calcines@visitscotland.com>
2023-04-06 16:33:18 +0100

    VS-224 FIX Change Environment variable to System Properties in Java

--- site/components/src/main/java/com/visitscotland/brxm/utils/Properties.java
+++ site/components/src/main/java/com/visitscotland/brxm/utils/Properties.java
@@ -367,6 +367,8 @@ public class Properties {
             logger.info("The property {} hasn't been set in the resourceBundle {}", key, bundleId);
         } else if (value.startsWith("$")){
             return getEnvironmentVariable(value.substring(1));
+        } else if (value.startsWith("%")){
+            return getSystemProperty(value.substring(1));
         } else {
             return value;
         }
@@ -380,5 +382,9 @@ public class Properties {
         } catch (RuntimeException e){
             return null;
         }
+    }
+
+    String getSystemProperty(String name){
+        return System.getProperty(name);
     }
 }
