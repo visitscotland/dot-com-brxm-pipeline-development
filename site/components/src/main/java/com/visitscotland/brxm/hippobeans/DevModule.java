67fdac0a085dce6fad9e956587e536019d36ca50
Jose Calcines <46224849+jcalcines@users.noreply.github.com>
2023-08-09 18:24:53 +0100

    PUFFIN-1 FEAT (Carbon Calculator) Add Field to the Dev Module

--- site/components/src/main/java/com/visitscotland/brxm/hippobeans/DevModule.java
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/DevModule.java
@@ -2,7 +2,6 @@ package com.visitscotland.brxm.hippobeans;
 
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
-
 import java.util.Calendar;
 
 @HippoEssentialsGenerated(internalName = "visitscotland:DevModule")
@@ -28,23 +27,8 @@ public class DevModule extends BaseDocument {
         return getMultipleProperty("visitscotland:footerContributions");
     }
 
-    @HippoEssentialsGenerated(internalName = "visitscotland:diff")
-    public String getDiff() {
-        return getSingleProperty("visitscotland:diff");
-    }
-
-    @HippoEssentialsGenerated(internalName = "visitscotland:translationDeadline")
-    public Calendar getTranslationDeadline() {
-        return getSingleProperty("visitscotland:translationDeadline");
-    }
-
-    @HippoEssentialsGenerated(internalName = "visitscotland:translationFlag")
-    public Boolean getTranslationFlag() {
-        return getSingleProperty("visitscotland:translationFlag");
-    }
-
-    @HippoEssentialsGenerated(internalName = "visitscotland:translationPriority")
-    public String getTranslationPriority() {
-        return getSingleProperty("visitscotland:translationPriority");
+    @HippoEssentialsGenerated(internalName = "visitscotland:freemarkerId")
+    public String getFreemarkerId() {
+        return getSingleProperty("visitscotland:freemarkerId");
     }
 }
