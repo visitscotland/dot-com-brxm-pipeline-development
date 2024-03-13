d4af95c1ce99c247980a9a379d8ab0bf8cf77005
jose.calcines <jcalcines@gmail.com>
2024-03-13 11:13:47 +0000

    #84 Create Back-end Implementation for Forms

--- site/components/src/main/java/com/visitscotland/brxm/factory/MarketoFormFactory.java
+++ site/components/src/main/java/com/visitscotland/brxm/factory/MarketoFormFactory.java
@@ -2,17 +2,34 @@ package com.visitscotland.brxm.factory;
 
 import com.visitscotland.brxm.hippobeans.MarketoForm;
 import com.visitscotland.brxm.model.MarketoFormModule;
+import com.visitscotland.brxm.model.form.MarketoConfiguration;
+import com.visitscotland.brxm.utils.Properties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MarketoFormFactory {
 
+
+    private final Properties properties;
+
+    public MarketoFormFactory(Properties properties){
+        this.properties = properties;
+    }
+
     public MarketoFormModule getModule(MarketoForm document) {
+        MarketoConfiguration cfg = new MarketoConfiguration();
+        cfg.setRecaptcha(properties.getProperty("form.recaptcha-key"));
+        cfg.setMarketoInstance(properties.getProperty("form.marketo.instance-url"));
+        cfg.setMunchkinId(properties.getProperty("form.marketo.munchkin"));
+        cfg.setScript(properties.getProperty("form.marketo.script"));
+
         MarketoFormModule module = new MarketoFormModule();
+        module.setConfig(cfg);
         module.setTitle(document.getTitle());
         module.setJsonUrl(document.getJsonUrl());
         module.setNoJavaScriptMessage(document.getNonJavaScriptMessage());
         module.setCopy(document.getCopy());
+
         return module;
     }
 
