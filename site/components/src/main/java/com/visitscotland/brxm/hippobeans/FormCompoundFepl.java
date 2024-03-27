350cc3e251bcc1503ad082b6b33bdebedbc5a011
jose.calcines <jcalcines@gmail.com>
2024-03-27 12:06:19 +0000

    Create new Form Document Type

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/FormCompoundFepl.java
@@ -0,0 +1,14 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:FormCompoundFepl")
+@Node(jcrType = "visitscotland:FormCompoundFepl")
+public class FormCompoundFepl extends HippoCompound {
+    @HippoEssentialsGenerated(internalName = "visitscotland:url")
+    public String getUrl() {
+        return getSingleProperty("visitscotland:url");
+    }
+}
