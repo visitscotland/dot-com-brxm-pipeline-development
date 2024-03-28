c3dbf3dcdd1499a65cf3a1c9e3670c23d26adbd8
jose.calcines <jcalcines@gmail.com>
2024-03-28 10:20:14 +0000

    #93 Create new unit tests

--- site/components/src/test/java/com/visitscotland/brxm/factory/FactoryTest.java
+++ site/components/src/test/java/com/visitscotland/brxm/factory/FactoryTest.java
@@ -2,8 +2,10 @@ package com.visitscotland.brxm.factory;
 
 import com.visitscotland.brxm.hippobeans.MarketoForm;
 import com.visitscotland.brxm.mock.MarketoFormMockBuilder;
-import com.visitscotland.brxm.model.MarketoFormModule;
+import com.visitscotland.brxm.model.FormModule;
+import com.visitscotland.brxm.model.form.MarketoConfiguration;
 import com.visitscotland.brxm.utils.Properties;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,13 +18,13 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-public class MarketoFactoryTest {
+class FactoryTest {
 
     @Mock
     Properties properties;
 
     @InjectMocks
-    MarketoFormFactory marketoFormFactory;
+    FormFactory marketoFormFactory;
 
     @Test
     @DisplayName("VS-3358 - Marketo form")
@@ -35,16 +37,38 @@ public class MarketoFactoryTest {
         when(properties.getProperty("form.marketo.munchkin")).thenReturn("munchkin");
         when(properties.getProperty("form.marketo.script")).thenReturn("script.js");
 
-        MarketoFormModule module = marketoFormFactory.getModule(form);
+        FormModule module = marketoFormFactory.getModule(form);
 
         assertEquals("title", module.getTitle());
         assertEquals("config", module.getJsonUrl());
         assertEquals("nojs", module.getNoJavaScriptMessage());
         assertEquals("copy", module.getCopy().getContent());
-        assertEquals("recaptcha", module.getConfig().getRecaptcha());
-        assertEquals("url", module.getConfig().getMarketoInstance());
-        assertEquals("script.js", module.getConfig().getScript());
-        assertEquals("munchkin", module.getConfig().getMunchkinId());
+        assertInstanceOf(MarketoConfiguration.class, module.getConfig());
+        assertEquals("recaptcha", ((MarketoConfiguration)module.getConfig()).getRecaptcha());
+        assertEquals("url", ((MarketoConfiguration)module.getConfig()).getMarketoInstance());
+        assertEquals("script.js", ((MarketoConfiguration)module.getConfig()).getScript());
+        assertEquals("munchkin", ((MarketoConfiguration)module.getConfig()).getMunchkinId());
+    }
+
+    @Test
+    @Disabled("To be completed")
+    @DisplayName("BE - #93 - Fepl Form Configuration")
+    void formMarketo() {
+
+    }
+
+    @Test
+    @Disabled("To be completed")
+    @DisplayName("BE - #93 - Marketo Form Configuration")
+    void formFepl() {
+
+    }
+
+    @Test
+    @Disabled("To be completed")
+    @DisplayName("BE - #93 - No configuration logs a warning")
+    void noConfiguration() {
+
     }
 
 }
