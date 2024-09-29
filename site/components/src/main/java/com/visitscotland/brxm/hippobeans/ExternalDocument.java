a499c0431fe5ef7210049a476bb6a531191bce51
jose.calcines <jcalcines@gmail.com>
2024-09-29 20:14:29 +0100

    DS-441 Make File link to use the same functionality of external documents in Shared Links

--- site/components/src/main/java/com/visitscotland/brxm/hippobeans/ExternalDocument.java
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ExternalDocument.java
@@ -1,12 +1,13 @@
 package com.visitscotland.brxm.hippobeans;
 
+import com.visitscotland.brxm.hippobeans.capabilities.UrlLink;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
 
 @HippoEssentialsGenerated(internalName = "visitscotland:ExternalDocument")
 @Node(jcrType = "visitscotland:ExternalDocument")
-public class ExternalDocument extends HippoCompound {
+public class ExternalDocument extends HippoCompound implements UrlLink {
     @HippoEssentialsGenerated(internalName = "visitscotland:category")
     public String getCategory() {
         return getSingleProperty("visitscotland:category");
