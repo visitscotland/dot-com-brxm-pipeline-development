db01ed61988d7c05e7fd27d676ac670067e0fa25
jose.calcines <jcalcines@gmail.com>
2024-10-02 17:14:51 +0100

    DS-494 Generate Beans

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ArticleBSH.java
@@ -0,0 +1,18 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:ArticleBSH")
+@Node(jcrType = "visitscotland:ArticleBSH")
+public class ArticleBSH extends Article {
+    @HippoEssentialsGenerated(internalName = "visitscotland:nested")
+    public Boolean getNested() {
+        return getSingleProperty("visitscotland:nested");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:theme")
+    public String getTheme() {
+        return getSingleProperty("visitscotland:theme");
+    }
+}
