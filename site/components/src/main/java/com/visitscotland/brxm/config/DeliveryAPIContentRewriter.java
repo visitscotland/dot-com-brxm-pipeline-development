ba4782357d5bb14cb51d3031bff5ac8bf0a915d9
jose.calcines <jcalcines@gmail.com>
2024-10-09 14:35:47 +0100

    BSHUB-98 Adapt to new article implementation

--- site/components/src/main/java/com/visitscotland/brxm/config/DeliveryAPIContentRewriter.java
+++ site/components/src/main/java/com/visitscotland/brxm/config/DeliveryAPIContentRewriter.java
@@ -14,6 +14,7 @@ import javax.jcr.RepositoryException;
  */
 public class DeliveryAPIContentRewriter extends HtmlContentRewriter {
 
+    static final String NESTED = "visitscotland:nested"
     private final HTMLtoVueTransformer transformer;
 
     public DeliveryAPIContentRewriter() throws Exception {
@@ -39,8 +40,9 @@ public class DeliveryAPIContentRewriter extends HtmlContentRewriter {
         try {
             if (node != null) {
                 parentDocument = node.getPath();
-                if (parentDocument.endsWith("/visitscotland:paragraph/visitscotland:copy")) {
-                    nested = node.getParent().getParent().getProperty("visitscotland:nest").getBoolean();
+                if (parentDocument.endsWith("/visitscotland:paragraph/visitscotland:copy")
+                        && node.getParent().getParent().hasProperty(NESTED)) {
+                    nested = node.getParent().getParent().getProperty(NESTED).getBoolean();
                 }
             } else {
                 parentDocument = "DeliveryAPIContentRewriter";
