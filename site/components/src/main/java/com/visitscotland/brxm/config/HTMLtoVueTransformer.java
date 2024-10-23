a72c8571f0c6120404d3f7bf3dce13995c2806ce
mprosser <Michael.Prosser@visitscotland.com>
2024-10-23 11:44:40 +0100

    feat(component library): update heading names in the transformer

--- site/components/src/main/java/com/visitscotland/brxm/config/HTMLtoVueTransformer.java
+++ site/components/src/main/java/com/visitscotland/brxm/config/HTMLtoVueTransformer.java
@@ -18,7 +18,7 @@ public class HTMLtoVueTransformer {
     private final LinkService linkService;
 
     private static final String[] HEADING_STYLES = {
-            "xl","l","m","s","xs","xxs"
+            "heading-xxl","heading-xl","heading-l","heading-m","heading-s","heading-xs"
     };
 
     HTMLtoVueTransformer(LinkService linkService){
