4cc850570ebe2eda6e3295fbb1c09ca369bb5831
Martin.Taylor <martin.taylor@visitscotland.com>
2024-11-08 15:16:53 +0000

    [VS-355] Default module position to bottom

--- site/components/src/main/java/com/visitscotland/brxm/factory/ProductSearchWidgetFactory.java
+++ site/components/src/main/java/com/visitscotland/brxm/factory/ProductSearchWidgetFactory.java
@@ -1,26 +1,22 @@
 package com.visitscotland.brxm.factory;
 
-import com.visitscotland.brxm.components.content.GeneralContentComponent;
-import com.visitscotland.brxm.components.content.PageContentComponent;
-import com.visitscotland.brxm.dms.DMSConstants;
-import com.visitscotland.brxm.dms.LocationLoader;
-import com.visitscotland.brxm.dms.PSType;
+import com.visitscotland.brxm.services.ResourceBundleService;
 import com.visitscotland.brxm.dms.model.LocationObject;
 import com.visitscotland.brxm.hippobeans.Destination;
-import com.visitscotland.brxm.hippobeans.General;
-import com.visitscotland.brxm.hippobeans.Page;
-import com.visitscotland.brxm.model.PSModule;
-import com.visitscotland.brxm.services.ResourceBundleService;
 import com.visitscotland.brxm.utils.CMSProperties;
+import com.visitscotland.brxm.dms.LocationLoader;
+import com.visitscotland.brxm.dms.DMSConstants;
+import com.visitscotland.brxm.hippobeans.Page;
 import com.visitscotland.brxm.utils.Language;
-import com.visitscotland.utils.Contract;
+import com.visitscotland.brxm.model.PSModule;
+import com.visitscotland.brxm.dms.PSType;
+
 import org.hippoecm.hst.core.component.HstRequest;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+import org.jetbrains.annotations.NotNull;
 
 @Component
 public class ProductSearchWidgetFactory {
-
     static final String BUNDLE_ID = "product-search-widget";
 
     final ResourceBundleService bundle;
@@ -50,29 +46,29 @@ public class ProductSearchWidgetFactory {
 
         // Non-JavaScript fall-back URL
         module.setSearchUrl(properties.getDmsHost() + Language.getLanguageForLocale(request.getLocale()).getPathVariable() + String.format(DMSConstants.PRODUCT_SEARCH, type.getPathVariable()));
-        module.setPosition(calculatePosition(request));
+        module.setPosition(POSITION_BOTTOM);
 
         return module;
     }
 
-    private String calculatePosition(HstRequest request){
-        Page page = (Page) request.getAttribute(PageContentComponent.DOCUMENT);
-        if (page instanceof General){
-            General general = ((General) page);
-
-            if (general.getBlog() != null) {
-                return POSITION_BOTTOM;
-            } else if (!Contract.isEmpty(general.getPswPosition()) && !general.getPswPosition().equals(POSITION_DEFAULT)) {
-                return general.getPswPosition();
-            } else if (!general.getTheme().equals(GeneralContentComponent.SIMPLE)) {
-                return POSITION_TOP;
-            }
-        } else if (page instanceof Destination){
-            return POSITION_TOP;
-        }
-
-        return POSITION_BOTTOM;
-    }
+//    private String calculatePosition(HstRequest request){
+//        Page page = (Page) request.getAttribute(PageContentComponent.DOCUMENT);
+//        if (page instanceof General){
+//            General general = ((General) page);
+//
+//            if (general.getBlog() != null) {
+//                return POSITION_BOTTOM;
+//            } else if (!Contract.isEmpty(general.getPswPosition()) && !general.getPswPosition().equals(POSITION_DEFAULT)) {
+//                return general.getPswPosition();
+//            } else if (!general.getTheme().equals(GeneralContentComponent.SIMPLE)) {
+//                return POSITION_TOP;
+//            }
+//        } else if (page instanceof Destination){
+//            return POSITION_TOP;
+//        }
+//
+//        return POSITION_BOTTOM;
+//    }
 
     /**
      * @param request the HstRequest request
